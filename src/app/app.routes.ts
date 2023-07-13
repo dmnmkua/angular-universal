a53db26273a08ef5a1039e0bf8875a70dddbfaf2
Maksym Drobakha <mdrobakha@geeksforless.com>
2023-07-13 22:42:01 +0300

    changed routes type

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { FirstComponent } from './pages/first/first.component';
+import { SecondComponent } from './pages/second/second.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)},
-  { path: 'first', loadComponent: () => import('./pages/first/first.component').then(m => m.FirstComponent)},
-  { path: 'second', loadComponent: () => import('./pages/second/second.component').then(m => m.SecondComponent)},
-  { path: 'about', loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent)},
+  { path: 'home', component: HomeComponent},
+  { path: 'first', component: FirstComponent},
+  { path: 'second', component: SecondComponent},
+  { path: 'about', component: AboutComponent},
 ];
